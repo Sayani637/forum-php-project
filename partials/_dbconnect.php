fdc47eba6570b8cec55c55e8feb3f7f074fed7c8
Sayani637 <paulsayani304@gmail.com>
2023-04-06 21:33:02 +0530

    forom php project

---
+++ partials/_dbconnect.php
@@ -0,0 +1,11 @@
+<?php
+// Script to connect datebase
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "idiscuss";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+
+?>
